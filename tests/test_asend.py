18b30b051bfd1517bfdda9479d6aaf2f8aecdfdf
KirillYabl <kirill-yablunovskii@mail.ru>
2023-06-27 06:23:51 -0300

    Add document request tests

--- tests/test_asend.py
+++ tests/test_asend.py
@@ -61,3 +61,50 @@ async def test_photo_request(
         response = await tg_request.asend()
         assert get_message_response == json.loads(json_payload)
         assert get_message_response == response.dict()
+
+
+@pytest.mark.anyio
+async def test_document_request_mocking(
+    httpx_mock: pytest_httpx.HTTPXMock,
+    get_document_response: tg_methods.SendDocumentResponse,
+):
+    tg_types.Chat.update_forward_refs()
+    tg_types.Message.update_forward_refs()
+    httpx_mock.add_response(
+        url='https://api.telegram.org/bottoken/sendDocument',
+        method='POST',
+        headers={
+            'content-type': 'application/json',
+            'accept': 'application/json',
+        },
+        json=get_document_response,
+    )
+
+    objects_which_pydantic_transforms_to_bytes_or_iterable_bytes = [
+        bytes('document content', encoding='utf8'),
+        b'document content',
+        '123',
+        123,
+    ]
+
+    for obj in objects_which_pydantic_transforms_to_bytes_or_iterable_bytes:
+        async with tg_methods.AsyncTgClient.setup('token'):
+            tg_request = tg_methods.SendBytesDocumentRequest(chat_id=1234567890, document=obj, filename='filename.csv')
+            json_payload = await tg_request.apost_as_json('sendDocument')
+            response = await tg_request.asend()
+            assert get_document_response == json.loads(json_payload)
+            assert get_document_response == response.dict()
+
+    objects_which_pydantic_transforms_to_str = [
+        'document content',
+        b'document content',
+        123,
+    ]
+
+    for obj in objects_which_pydantic_transforms_to_str:
+        async with tg_methods.AsyncTgClient.setup('token'):
+            tg_request = tg_methods.SendUrlDocumentRequest(chat_id=1234567890, document=obj, filename='filename.csv')
+            json_payload = await tg_request.apost_as_json('sendDocument')
+            response = await tg_request.asend()
+            assert get_document_response == json.loads(json_payload)
+            assert get_document_response == response.dict()
--- tests/test_send.py
+++ tests/test_send.py
@@ -57,3 +57,49 @@ def test_message_request(
         assert isinstance(response, tg_methods.SendMessageResponse)
         assert get_message_response == json.loads(json_payload)
         assert get_message_response == response.dict()
+
+
+def test_document_request_mocking(
+    httpx_mock: pytest_httpx.HTTPXMock,
+    get_document_response: tg_methods.SendDocumentResponse,
+):
+    tg_types.Chat.update_forward_refs()
+    tg_types.Message.update_forward_refs()
+    httpx_mock.add_response(
+        url='https://api.telegram.org/bottoken/sendDocument',
+        method='POST',
+        headers={
+            'content-type': 'application/json',
+            'accept': 'application/json',
+        },
+        json=get_document_response,
+    )
+
+    objects_which_pydantic_transforms_to_bytes_or_iterable_bytes = [
+        bytes('document content', encoding='utf8'),
+        b'document content',
+        '123',
+        123,
+    ]
+
+    for obj in objects_which_pydantic_transforms_to_bytes_or_iterable_bytes:
+        with tg_methods.SyncTgClient.setup('token'):
+            tg_request = tg_methods.SendBytesDocumentRequest(chat_id=1234567890, document=obj, filename='filename.csv')
+            json_payload = tg_request.post_as_json('sendDocument')
+            response = tg_request.send()
+            assert get_document_response == json.loads(json_payload)
+            assert get_document_response == response.dict()
+
+    objects_which_pydantic_transforms_to_str = [
+        'document content',
+        b'document content',
+        123,
+    ]
+
+    for obj in objects_which_pydantic_transforms_to_str:
+        with tg_methods.SyncTgClient.setup('token'):
+            tg_request = tg_methods.SendUrlDocumentRequest(chat_id=1234567890, document=obj, filename='filename.csv')
+            json_payload = tg_request.post_as_json('sendDocument')
+            response = tg_request.send()
+            assert get_document_response == json.loads(json_payload)
+            assert get_document_response == response.dict()
