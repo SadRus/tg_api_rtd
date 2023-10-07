e6e747f6e8845e69b647f998de0a1edacef69749
Алексей Милов <killerarz@yandex.ru>
2023-10-08 00:21:02 +0300

    Added test with raised errors

---
+++ tests/test_fail_send.py
@@ -0,0 +1,203 @@
+import json
+import typing
+
+import pytest
+import pytest_httpx
+import requests
+from pydantic import ValidationError
+
+from tg_api import tg_methods, tg_types
+
+
+def test_photo_request_mocking_with_large_caption(
+    get_photo_response: dict[str, typing.Any],
+):
+    tg_types.Chat.update_forward_refs()
+    tg_types.Message.update_forward_refs()
+
+    photo_url = 'https://memepedia.ru/wp-content/uploads/2018/06/kto-prochital-tot-sdohnet.jpg'
+    response = requests.get(photo_url)
+    response.raise_for_status()
+
+    with tg_methods.SyncTgClient.setup('token'):
+        with pytest.raises(ValidationError):
+            tg_methods.SendBytesPhotoRequest(
+                chat_id=1234567890,
+                caption='a' * 1025,
+                photo=str(response.content),
+                caption_entities=[tg_types.MessageEntity(type='123', offset=1, length=1)],
+                reply_markup=tg_types.InlineKeyboardMarkup(
+                    inline_keyboard=[
+                        [tg_types.InlineKeyboardButton(text='123')],
+                    ],
+                ),
+            )
+
+    with tg_methods.SyncTgClient.setup('token'):
+        with pytest.raises(ValidationError):
+            tg_methods.SendUrlPhotoRequest(
+                chat_id=1234567890,
+                photo=photo_url,
+                caption='a' * 1025,
+                caption_entities=[tg_types.MessageEntity(type='123', offset=1, length=1)],
+                reply_markup=tg_types.InlineKeyboardMarkup(
+                    inline_keyboard=[
+                        [tg_types.InlineKeyboardButton(text='123')],
+                    ],
+                ),
+            )
+
+
+def test_message_request_with_large_text(
+    httpx_mock: pytest_httpx.HTTPXMock,
+    get_message_response: dict[str, typing.Any],
+):
+    with tg_methods.SyncTgClient.setup('token'):
+        with pytest.raises(ValidationError):
+            tg_methods.SendMessageRequest(
+                chat_id=1234567890,
+                text='a' * 4097
+            )
+
+
+def test_document_request_mocking_with_large_caption(
+    get_document_response: dict[str, typing.Any],
+):
+    tg_types.Chat.update_forward_refs()
+    tg_types.Message.update_forward_refs()
+
+    objects_which_pydantic_transforms_to_bytes_or_iterable_bytes = [
+        bytes('document content', encoding='utf8'),
+        b'document content',
+        '123',
+        123,
+    ]
+
+    for obj in objects_which_pydantic_transforms_to_bytes_or_iterable_bytes:
+        with tg_methods.SyncTgClient.setup('token'):
+            with pytest.raises(ValidationError):
+                tg_methods.SendBytesDocumentRequest(
+                    chat_id=1234567890,
+                    caption="a" * 1025,
+                    document=obj,
+                    filename='filename.csv',
+                    caption_entities=[tg_types.MessageEntity(type='123', offset=1, length=1)],
+                    reply_markup=tg_types.InlineKeyboardMarkup(
+                        inline_keyboard=[
+                            [tg_types.InlineKeyboardButton(text='123')],
+                        ],
+                    ),
+                )
+
+    objects_which_pydantic_transforms_to_str = [
+        'document content',
+        b'document content',
+        123,
+    ]
+
+    for obj in objects_which_pydantic_transforms_to_str:
+        with tg_methods.SyncTgClient.setup('token'):
+            with pytest.raises(ValidationError):
+                tg_methods.SendUrlDocumentRequest(
+                    chat_id=1234567890,
+                    caption="a" * 1025,
+                    document=obj,
+                    filename='filename.csv',
+                    caption_entities=[tg_types.MessageEntity(type='123', offset=1, length=1)],
+                    reply_markup=tg_types.InlineKeyboardMarkup(
+                        inline_keyboard=[
+                            [tg_types.InlineKeyboardButton(text='123')],
+                        ],
+                    ),
+                )
+
+
+def test_edit_message_text_request_mocking_with_large_text(
+    edit_message_text_response: dict[str, typing.Any],
+    keyboard: tg_types.InlineKeyboardMarkup,
+):
+    with tg_methods.SyncTgClient.setup('token'):
+        with pytest.raises(ValidationError):
+            tg_methods.EditMessageTextRequest(
+                chat_id=1234567890,
+                message_id=12345,
+                text='a' * 4097,
+            )
+
+
+def test_edit_message_caption_request_mocking_with_large_caption(
+    edit_message_caption_response: dict[str, typing.Any],
+):
+    with tg_methods.SyncTgClient.setup('token'):
+        with pytest.raises(ValidationError):
+            tg_methods.EditMessageCaptionRequest(
+                chat_id=1234567890,
+                message_id=12345,
+                caption='a' * 1025,
+            )
+
+
+def test_edit_message_media_url_photo_request_mocking_with_large_caption(
+    edit_message_media_photo_response: dict[str, typing.Any],
+):
+    with tg_methods.SyncTgClient.setup('token'):
+        with pytest.raises(ValidationError):
+            media = tg_types.InputMediaUrlPhoto(
+                media='https://link_to_photo.jpg',
+                caption='a' * 1025,
+            )
+            tg_methods.EditUrlMessageMediaRequest(
+                chat_id=1234567890,
+                message_id=12345,
+                media=media,
+            )
+
+
+def test_edit_message_media_url_document_request_mocking(
+    edit_message_media_document_response: dict[str, typing.Any],
+):
+    with tg_methods.SyncTgClient.setup('token'):
+        with pytest.raises(ValidationError):
+            media = tg_types.InputMediaUrlDocument(
+                media='https://link_to_document.pdf',
+                caption='a' * 1025,
+            )
+            tg_methods.EditUrlMessageMediaRequest(
+                chat_id=1234567890,
+                message_id=12345,
+                media=media,
+            )
+
+def test_edit_message_media_bytes_photo_request_mocking(
+    edit_message_media_photo_response: dict[str, typing.Any],
+):
+    with tg_methods.SyncTgClient.setup('token'):
+        with pytest.raises(ValidationError):
+            media = tg_types.InputMediaBytesPhoto(
+                media='attach://attachement.jpg',
+                media_content=b'photo content',
+                caption='a' * 1025,
+            )
+            tg_methods.EditBytesMessageMediaRequest(
+                chat_id=1234567890,
+                message_id=12345,
+                media=media,
+            )
+
+
+
+def test_edit_message_media_bytes_document_request_mocking(
+    edit_message_media_document_response: dict[str, typing.Any],
+):
+    with tg_methods.SyncTgClient.setup('token'):
+        with pytest.raises(ValidationError):
+            media = tg_types.InputMediaBytesDocument(
+                media='attach://attachement.pdf',
+                media_content=b'document content',
+                caption='a' * 1025,
+            )
+            tg_methods.EditBytesMessageMediaRequest(
+                chat_id=1234567890,
+                message_id=12345,
+                media=media,
+            )
